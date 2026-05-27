aed6a7de57df78db2efa4745bcf86ca1c3aaafb0
thunhorngork <othunhorng@gmail.com>
2026-05-27 09:24:08 +0700

    delete-select-bank-vattanac-bank

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -146,11 +146,7 @@ const Update = () => {
             <label class="form-check-label" for="kbprask"><img src="https://i.pinimg.com/1200x/06/e9/4c/06e94cc0a6c032b6ce65a67b32ed4bf7.jpg" alt="KB PRASAK Bank" width="30px" class="me-2 bank-img ">KB PRASAK Bank</label>
         </div>
 
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="bank" value="Vattanac Bank" id="nbc">
-            <label class="form-check-label" for="nbc"><img src="https://scontent.fpnh5-1.fna.fbcdn.net/v/t39.30808-6/432020533_801841341974812_467067171358853087_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeFr6PWiqruezyd3UBxVj1tl9ZqK44-H4Ur1morjj4fhSuIqVHRAOeaOMfgN2kKa_zAcEkAurzpJSlpAySSaych6&_nc_ohc=5nQahe35Mp8Q7kNvwFHWW2L&_nc_oc=Adrim-gCcdOnjshu0_OQ6VJYRLARGBI0zXF2_69pnj5D2rzLmUmiaIIjYAd2yNYcNjM&_nc_zt=23&_nc_ht=scontent.fpnh5-1.fna&_nc_gid=vpNtEkaghio4zBIrcG_N3g&_nc_ss=7b2a8&oh=00_Af5JCm5jssur32ZvsUBtmirdKoEbsDKWlJ_1c_2MZ9qqZQ&oe=6A10719F" alt="Vattanac Bank" width="30px" class="me-2 bank-img ">Vattanac Bank</label>
-        </div>
-
+        
         <div class="form-check">
             <input class="form-check-input" type="radio" name="bank" value="Cash on Delivery" id="cod">
             <label class="form-check-label" for="cod"><img src="https://i.pinimg.com/736x/fd/5d/36/fd5d360ff078dc897e88dbed7c95118e.jpg" alt="COD" width="30px" class="me-2 bank-img ">Cash on Delivery</label>
